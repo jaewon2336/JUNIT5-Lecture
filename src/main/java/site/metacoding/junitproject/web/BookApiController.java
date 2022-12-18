4898d4840f29d9f3fb1bfbcde710b4d632427317
jaewon <xldzjqpf1588@naver.com>
2022-12-18 21:09:23 +0900

    BookApiController 본코드 - 책한건보기, 책삭제하기 완료

--- src/main/java/site/metacoding/junitproject/web/BookApiController.java
+++ src/main/java/site/metacoding/junitproject/web/BookApiController.java
@@ -9,7 +9,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -56,13 +58,18 @@ public class BookApiController {
     }
 
     // 3. 책한건보기
-    public ResponseEntity<?> getBookOne() {
-        return null;
+    @GetMapping("/api/v1/book/{id}")
+    public ResponseEntity<?> getBookOne(@PathVariable Long id) {
+        BookRespDto bookRespDto = bookService.책한건보기(id);
+        return new ResponseEntity<>(CMRespDto.builder().code(1).msg("글 한건보기 성공").body(bookRespDto).build(),
+                HttpStatus.OK);
     }
 
     // 4. 책삭제하기
-    public ResponseEntity<?> deleteBook() {
-        return null;
+    @DeleteMapping("/api/v1/book/{id}")
+    public ResponseEntity<?> deleteBook(@PathVariable Long id) {
+        bookService.책삭제하기(id);
+        return new ResponseEntity<>(CMRespDto.builder().code(1).msg("글 삭제하기 성공").build(), HttpStatus.OK);
     }
 
     // 5. 책수정하기
